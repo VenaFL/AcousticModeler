3326590b56fa85edb5ad24a0e529a9429047af00
Brian <brianmtran@outlook.com>
2023-12-03 22:14:06 -0500

    Added the conversion from mp3 to wav and displays waveform.

--- gui.py
+++ gui.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+plt.figure(figsize=(14, 10))
 
 class AudioView:
     def __init__(self, root, controller):
@@ -25,4 +29,20 @@ class AudioView:
         formatted_duration = "{:.2f}".format(duration_seconds)
         self.duration_label.config(text=f"Duration: {formatted_duration} seconds")
 
+    def show_graph(self, time):
+        wav_obj = wave.open(file_path, 'rb')
+
+        sample_freq = wav_obj.getframerate()
+        n_samples = wav_obj.getnframes()
+        t_audio = n_samples / sample_freq
+        signal_wave = wav_obj.readframes(n_samples)
+        signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+        times = np.linspace(0, n_samples / sample_freq, num=n_samples)
+        plt.figure(figsize=(15, 5))
+        plt.plot(times, l_channel)
+        plt.title('Left Channel')
+        plt.ylabel('Signal Value')
+        plt.xlabel('Time (s)')
+        plt.xlim(0, t_audio)
+        plt.show()
 
--- model.py
+++ model.py
@@ -7,12 +7,21 @@ class AudioModel:
         self.wav_path = None
 
     def load_audio_file(self, file_path):
+        # Takes the audio's file path and separates the extension before attaching .wav onto it
         output_filepath = os.path.splitext(file_path)[0] + '.wav'
+        # Assigns the audio file's extension to the variable "extension"
         _, extension = os.path.splitext(file_path)
+        # Makes the extension lowercase
         file_type = extension.lower()
 
-        self.audio_data = AudioSegment.from_wav(file_path)
-        self.wav_path = file_path
+        if extension == '.mp3':
+            # Takes the mp3 file sound
+            audio_data = AudioSegment.from_mp3(file_path)
+            # Exports it to output_filepath as a wav file
+            audio_data.export(output_filepath, format='wav')
+
+        self.audio_data = AudioSegment.from_wav(output_filepath)
+        self.wav_path = output_filepath
 
     def strip_metadata(self):
         if self.audio_data:
